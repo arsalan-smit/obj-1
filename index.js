4bb643adbd6e99205436b52dc58ac4d399acfea3
arsalan-smit <arsalangg05@gmail.com>
2024-10-21 18:17:07 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+alert("Welcome to our website!");
+
+alert("Error! please enter the valid password.");
+
+alert("Welcome to JS Land \nHappy Coding.");
+
+alert("Welcome to JS Land ");
+alert("Happy Coding");
+
+console.log("hello i can run js through my web brower's console.");
+
+
